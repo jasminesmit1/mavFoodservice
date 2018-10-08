40ec51c258603d99aa89e2a0f28707149a23c7eb
Jasmine Smith <jasminesmit1@unomaha.edu>
2018-10-08 13:54:56 -0500

    final commit

--- mavFoodservice/urls.py
+++ mavFoodservice/urls.py
@@ -16,6 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('crm.urls')),
+    #url(r'^login/$', views.login, name='login'),
+    #url(r'^logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
 ]
--- mavFoodservice/wsgi.py
+++ mavFoodservice/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mavFoodservice project.
+WSGI config for mavFood project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mavFoodservice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mavFood.settings')
 
 application = get_wsgi_application()
+
