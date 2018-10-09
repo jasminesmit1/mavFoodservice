e07b7678f393b4b328dd508a225b88f7eb5fd37f
Jasmine Smith <jasminesmit1@unomaha.edu>
2018-10-08 19:34:03 -0500

    updated Template directory in settings

--- mavFoodservice/settings.py
+++ mavFoodservice/settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'mavFoodservice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'crm/Templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
